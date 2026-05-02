f16cb66c6c19ad4a88dc86d6c509592904773e94
root <root@srv1581101.hstgr.cloud>
2026-05-02 12:44:33 -0400

    rollback test

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="/opt/app/templates")
 
-# Ana sayfa
 @app.route("/")
 def home():
     return render_template("index.html")
 
-# Health check
 @app.route("/health")
 def health():
     return "OK", 200
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
