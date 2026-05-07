f3c44ee5aaa95a8425e314fce86361e8ac0f75e7
root <root@srv1581101.hstgr.cloud>
2026-05-07 16:07:44 -0400

    rollback test

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask
 
-app = Flask(__name__, template_folder="/opt/app/templates")
+app = Flask(__name__)
 
+# Ana endpoint
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return "Seni çok seviyorum Duyguu7 <3 🚀"
 
+# Health check endpoint (CI/CD için kritik)
 @app.route("/health")
 def health():
     return "OK", 200
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
